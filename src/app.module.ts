b1af1a3f8038caa4918c19cc4217faff4abff67d
Denis Chumachenko <deniskachumachenko1@gmail.com>
2023-05-03 04:19:17 +0300

    Trying to implement authorization (without jwt yet)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from '@/src/users/users.module';
+import { AuthModule } from '@/src/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
